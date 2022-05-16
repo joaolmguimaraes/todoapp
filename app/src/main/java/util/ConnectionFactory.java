97b03214dd3fbf8ebf8cb847d02b6a458521e2a1
joaolmguimaraes <jlmguimaraes@live.com>
2022-05-16 14:55:53 -0300

    First commit

---
+++ app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author jlmgu
+ */
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return (Connection) DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao conectar", e);
+        }
+    }
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão.");
+        }
+    }
+    
+      public static void closeConnection(Connection connection, 
+              PreparedStatement statement) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão.");
+        }
+    }
+      
+       public static void closeConnection(Connection connection, 
+              PreparedStatement statement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if(resultSet != null) {
+                resultSet.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão.");
+        }
+    }
+
+   
+    
+    
+}
